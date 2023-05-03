3265cf840d950e1ddb2fa77560ee718661de5114
Pridestalkerr <danartenie158@gmail.com>
2023-05-03 11:08:33 +0300

    tw theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,25 @@
 import { type Config } from "tailwindcss";
 
+const colors = {
+  darker: "#222831",
+  dark: "#393E46",
+  yellow: "#FFD369",
+  white: "#EEEEEE",
+} as const;
+
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: colors,
+      textColor: colors,
+      borderColor: colors,
+      hover: {
+        backgroundColor: colors,
+        textColor: colors,
+        borderColor: colors,
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
